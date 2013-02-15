44c87db31c2b61d78c389f079fe572a943a3b888
Gonghan Wang <wanggonghan@gmail.com>
2013-02-15 16:49:40 -0700

    2/15
    commit

---
+++ src/neo4j/assignment3.java
@@ -0,0 +1,12 @@
+package neo4j;
+
+public class assignment3 {
+
+	public void run(){
+		GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
